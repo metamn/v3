aa8857591543f67a48598748ab30d24afa6ce969
metamn <bartus.csongor@gmail.com>
2012-03-07 20:10:41 +0200

    Vanilla Rails app added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+V3::Application.config.secret_token = '921aa52921e3444b97eb82040eba519ddd66acbf53e13783550e2e3e76120508fe6939ea19781722d5275bb0927e9c10cc21918d2d2dfb815f37ca8fbd456ac4'
