84a806804ecbfe5304945c40db56c0e820c09869
metamn <bartus.csongor@gmail.com>
2012-03-09 13:13:50 +0200

    Rails and Spree removed. Starting from scratch with a Jekyll based, design only site

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-V3::Application.config.secret_token = '1ef50ff0d0974d5f82868dd79bd6bb6a6b7755b58f20a2b181a0802578b2ed28dafb1f358c425b21eeaf9a7231f6760e866e439674fe0072585be184217bef92'
