02db2783963112a71388bce630e0eab30a7f2a88
metamn <bartus.csongor@gmail.com>
2012-03-13 12:13:19 +0200

    Scroll view added

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -3,10 +3,35 @@ jQuery.noConflict();
 jQuery(document).ready(function(){
   
   
-  // Show Search, Cart, Information on click
-  jQuery("header h2").click(function() {
-    jQuery(this).next().slideToggle();
-  });
+  // Product Scroll view
+  jQuery("#product.scroll").first().addClass('active');
   
+  jQuery("#product-scroll-right").click(function() {
+    var first = jQuery("#product.active");
+            
+    jQuery(first).removeClass('active');
+    jQuery(first).hide();
     
+    if (!(jQuery(first).next().hasClass('scroll'))) {
+      jQuery("#product.scroll").first().addClass('active');
+      jQuery("#product.scroll").first().show();
+    } else {
+      jQuery(first).next().addClass('active');
+      jQuery(first).next().show();
+    }
+  }); 
+  jQuery("#product-scroll-left").click(function() {
+    var first = jQuery("#product.active");
+            
+    jQuery(first).removeClass('active');
+    jQuery(first).hide();
+    
+    if (!(jQuery(first).prev().hasClass('scroll'))) {
+      jQuery("#product.scroll").last().addClass('active');
+      jQuery("#product.scroll").last().show();
+    } else {
+      jQuery(first).prev().addClass('active');
+      jQuery(first).prev().show();
+    }
+  }); 
 });
